0b461ddc4ad1efd0e6bb0962101aa85bd00fed94
Franklin Schrans <f.schrans@me.com>
2017-02-17 16:38:31 +0000

    Rearrange server

--- web/server.js
+++ web/server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const WebSocketServer = require('websocket').server;
+
 const config = {
   port: 3000
 }
+
+const server = app.listen(config.port, () => {
+    const {address, port} = server.address();
+    console.log(`The server is running at http://localhost:${port}/`);
+});
+
+const socket = new WebSocketServer({ httpServer : server });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve('public/index.html'))
-})
-
-app.listen(config.port, function () {
-    console.log('Server started')
 })
